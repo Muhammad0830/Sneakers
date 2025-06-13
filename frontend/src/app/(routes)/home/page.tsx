ffa1c16bc62e5211a4eb88e0fafe470401ecc254
Muhammad0830 <abduqayumovm1@gmail.com>
2025-06-13 16:10:07 +0500

    colors to globals and a font to the whole project added

--- frontend/src/app/(routes)/home/page.tsx
+++ frontend/src/app/(routes)/home/page.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from "react";
+import NavBar from "@/components/NavBar";
 
 const Home = () => {
-  
-  return (
-    <div>Home</div>
-  )
-}
 
-export default Home
+  return (
+    <div className="min-h-screen">
+      <NavBar></NavBar>
+      <div className="font-bold text-[#171717]">HOME</div>
+      <div className="text-black">HOME</div>
+    </div>
+  );
+};
+
+export default Home;
