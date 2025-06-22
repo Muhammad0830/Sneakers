12ef48c2aa7c7ade4b803777362e1416ef4b733e
Muhammad0830 <abduqayumovm1@gmail.com>
2025-06-22 14:44:38 +0500

    useApiQuery component added

--- frontend/src/app/[locale]/(routes)/home/page.tsx
+++ frontend/src/app/[locale]/(routes)/home/page.tsx
@@ -7,14 +7,8 @@ import Link from "next/link";
 import SVGpathComponent from "@/components/SVGpathComponent";
 import Image from "next/image";
 import SimpleButton from "@/components/ui/SimpleButton";
-import { useQuery } from "@tanstack/react-query";
-import { GetTrending } from "@/api/sneakers";
-
-interface Product {
-  id: number;
-  name: string;
-  size: string;
-}
+import useApiQuery from "@/hooks/useApiQuery";
+import { Product } from "@/types/types";
 
 const Home = () => {
   const t = useTranslations("Home");
@@ -30,10 +24,7 @@ const Home = () => {
     data: products = [],
     isLoading,
     error,
-  } = useQuery({
-    queryKey: ["sneakers"],
-    queryFn: () => GetTrending(),
-  });
+  } = useApiQuery<Product[]>("/trending", "Trending");
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
