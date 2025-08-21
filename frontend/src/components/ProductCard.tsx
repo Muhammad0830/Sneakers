dc370f447a6437335d8116f92884d275474727a5
Muhammad0830 <abduqayumovm1@gmail.com>
2025-08-21 15:59:57 +0500

    product card dark mode bug fixed

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -4,10 +4,12 @@ import React from "react";
 import { Product } from "@/types/types";
 import Image from "next/image";
 import { MessageCircle, Star, ThumbsUp } from "lucide-react";
+import { useTheme } from "next-themes";
 
 const ProductCard = ({ product }: { product: Product }) => {
   const colors = product.color.split(", ");
   const sizes = product.size.split(", ");
+  const { theme } = useTheme();
 
   const rating = Number(product.rating).toFixed(1);
 
@@ -31,13 +33,17 @@ const ProductCard = ({ product }: { product: Product }) => {
         </div>
 
         {/* gender badge */}
-        <div className="absolute z-30 capitalize top-3 cursor-default font-semibold text-sm left-3 bg-primary border-primary text-white dark:text-black px-1 rounded-sm">
+        <div className="absolute z-30 capitalize top-3 cursor-default font-semibold text-sm left-3 bg-primary border-primary text-white px-1 rounded-sm">
           {product.gender}
         </div>
 
         <div
           style={{
-            background: `linear-gradient(-45deg, white 0%, var(--color-primary-80) 50%, white 100%)`,
+            backgroundImage:
+              theme === "light"
+                ? `linear-gradient(-45deg, white 0%, var(--color-primary-80) 50%, white 100%)`
+                : `linear-gradient(-45deg, black 0%, var(--color-primary-80) 50%, black 100%)`,
+            backgroundColor: theme === "light" ? "" : "#222222",
           }}
           className="z-20 w-full relative overflow-hidden border border-primary rounded-md"
         >
@@ -54,10 +60,10 @@ const ProductCard = ({ product }: { product: Product }) => {
             className={`absolute flex flex-col gap-2 justify-center items-center left-0 right-0 bottom-[1rem] transition-transform duration-300 translate-y-[150%] group-hover:translate-y-[0%]`}
           >
             <div className="flex items-center gap-2 justify-center">
-              <div className="flex items-center gap-2 justify-center text-sm bg-white px-1 rounded-md">
+              <div className="flex items-center gap-2 justify-center text-sm bg-white dark:bg-black px-1 rounded-md">
                 <Star size={12} color="yellow" fill="yellow" /> {rating} / 5
               </div>
-              <div className="rounded-md bg-white px-1 text-sm">
+              <div className="rounded-md bg-white dark:bg-black px-1 text-sm">
                 {product.reviews.length} reviews
               </div>
             </div>
@@ -78,7 +84,7 @@ const ProductCard = ({ product }: { product: Product }) => {
             </div>
             <div className="flex items-center justify-center gap-1">
               {sizes.map((size, index) => (
-                <span className="bg-white px-1 rounded-md text-sm" key={index}>
+                <span className="bg-white dark:bg-black px-1 rounded-md text-sm" key={index}>
                   {size}
                 </span>
               ))}
@@ -90,15 +96,15 @@ const ProductCard = ({ product }: { product: Product }) => {
 
         <div className="absolute cardButtonsWrapper -translate-y-[0%] top-0 py-1 z-0 left-0 right-0 bg-background transition-transform duration-200 flex items-center justify-between">
           <div className="flex items-center gap-2 ml-[2px]">
-            <div className="p-1 cursor-pointer rounded-2xl cardButtons bg-white translate-y-[150%] transition-transform duration-300">
+            <div className="p-1 cursor-pointer rounded-2xl cardButtons bg-white text-black border-1 border-black/20 dark:border-transparent translate-y-[150%] transition-transform duration-300">
               <ThumbsUp size={16} color="black" />
             </div>
-            <div className="p-1 cursor-pointer rounded-2xl bg-white cardButtons translate-y-[150%] transition-transform duration-300 delay-75">
+            <div className="p-1 cursor-pointer rounded-2xl bg-white text-black border-1 border-black/20 dark:border-transparent cardButtons translate-y-[150%] transition-transform duration-300 delay-75">
               <MessageCircle size={16} color="black" />
             </div>
           </div>
 
-          <div className="flex cursor-pointer items-center gap-1 bg-white rounded-2xl px-2 py-0.5 mr-[2px] cardButtons translate-y-[150%] transition-transform duration-300 delay-150">
+          <div className="flex cursor-pointer items-center gap-1 bg-white text-black border-1 border-black/20 dark:border-transparent rounded-2xl px-2 py-0.5 mr-[2px] cardButtons translate-y-[150%] transition-transform duration-300 delay-150">
             <span className="text-sm">Quick Look</span>
           </div>
         </div>
