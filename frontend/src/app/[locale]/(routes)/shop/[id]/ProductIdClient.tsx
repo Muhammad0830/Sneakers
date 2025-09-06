af4bc07ba8642c0860355cb1c1c1aa75d23f4359
Muhammad0830 <abduqayumovm1@gmail.com>
2025-09-06 17:56:25 +0500

    product[id] page responsiveness is done

--- frontend/src/app/[locale]/(routes)/shop/[id]/ProductIdClient.tsx
+++ frontend/src/app/[locale]/(routes)/shop/[id]/ProductIdClient.tsx
@@ -1,6 +1,5 @@
 "use client";
 import RecommendedProducts from "@/components/RecommendedProducts";
-import Button from "@/components/ui/Button";
 import {
   ArrowLeft,
   ArrowRight,
@@ -19,6 +18,7 @@ const ProductIdClient = () => {
   const id = params?.id;
   const [selectedVariant, setSelectedVariant] = useState(0);
   const { theme } = useTheme();
+  const width = window.innerWidth;
 
   const product = {
     id: 1,
@@ -67,85 +67,158 @@ const ProductIdClient = () => {
   if (!id) return notFound();
 
   return (
-    <div className="mt-4 px-[60px]">
+    <div className="mt-4 lg:px-[60px] md:px-[40px] sm:px-[30px] px-[20px]">
       <div>
         PathName: <span>Home</span>
         <span>/Shop/{id}</span>
       </div>
 
       {/* product main page */}
-      <div className="relative w-full flex items-center gap-4 mb-4">
-        <div className="relative w-full min-h-[450px] mt-5 ">
-          <div className="absolute w-1/2 top-0">
-            {/* product variants */}
-            <div className="absolute top-0 left-0 right-0 flex items-center justify-center gap-2">
-              {product.variants.map((v, index) => (
-                <button
-                  key={index}
-                  onClick={() => {
-                    setSelectedVariant(index);
-                  }}
-                  className={`w-[80px] ${
-                    index === selectedVariant
-                      ? "shadow-[0px_0px_10px_2px_var(--primary)]"
-                      : ""
-                  } bg-white hover:shadow-[0px_0px_10px_2px_var(--primary)] dark:bg-black relative aspect-square border border-primary rounded-md cursor-pointer transition-all duration-300`}
-                >
-                  <Image
-                    src={`/sneakers-${index + 1}.png`}
-                    alt={product.title}
-                    fill
-                  />
+      <div className="relative w-full sm:flex items-center gap-4 mb-4 mt-5">
+        {/* product info card */}
+        <div className="sm:absolute z-20 h-full right-0 top-0 sm:w-1/2 w-full flex items-center justify-center">
+          <div className="sm:p-5 p-3 sm:mt-5 flex sm:w-auto sm:min-w-auto min-w-[clamp(280px,70vw,400px)] flex-col sm:gap-1 rounded-2xl dark:shadow-[0px_0px_15px_1px_#ffffff50] shadow-[0px_0px_15px_1px_#00000080] backdrop-blur-xs bg-white/70 dark:bg-black/70">
+            <div className="font-bold lg:text-2xl sm:text-xl text-[16px] flex justify-between">
+              <span>{product.title}</span>
+              <span className="sm:hidden inline-block">{product.price}$</span>
+            </div>
+            <div className="smLflex hidden items-center justify-between gap-2">
+              <div className="lg:text-3xl sm:text-xl font-bold">
+                {product.price}$
+              </div>
+              <div className="flex items-center gap-2 bg-primary rounded-full px-2">
+                <span className="font-bold lg:text-2xl sm:text-lg text-[16px] text-white">
+                  {product.rating}
+                </span>
+                <Star
+                  className="lg:block hidden"
+                  color="yellow"
+                  size={24}
+                  fill="yellow"
+                />
+                <Star
+                  className="lg:hidden block"
+                  color="yellow"
+                  size={18}
+                  fill="yellow"
+                />
+              </div>
+            </div>
+            <div className="flex items-center justify-between gap-2">
+              <div>
+                <div className="sm:text-[16px] text-[12px]">Choose a size</div>
+                <div className="flex gap-1 items-center mb-1">
+                  {product.sizes.map((size, index) => {
+                    return (
+                      <button
+                        className="px-1 py-0.5 lg:min-w-[30px] sm:min-w-[25px] min-w-[20px] border border-black/50 dark:border-white/50 sm:rounded-md rounded-sm lg:text-[16px] sm:text-[14px] text-[12px]"
+                        key={index}
+                      >
+                        {size}
+                      </button>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="flex items-center gap-2 bg-primary rounded-full px-2">
+                <span className="font-bold lg:text-2xl sm:text-lg text-[16px] text-white">
+                  {product.rating}
+                </span>
+                <Star
+                  className="lg:hidden block"
+                  color="yellow"
+                  size={18}
+                  fill="yellow"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="sm:text-[16px] text-[12px]">Choose a color</div>
+              <div className="flex items-center gap-1">
+                {product.colors.map((color, index) => {
+                  return (
+                    <button
+                      style={{ backgroundColor: `${color}` }}
+                      className={`sm:w-[25px] w-[20px] aspect-square border border-black/50 dark:border-white/50 sm:rounded-md rounded-sm`}
+                      key={index}
+                    ></button>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="flex items-center justify-between gap-2 lg:font-bold lg:text-lg sm:text-[16px] text-[14px]">
+              <span>
+                Delivery: <span className="text-primary">Free</span>
+              </span>
+              <span>For {product.gender}</span>
+            </div>
+            <div className="flex sm:justify-center justify-between items-center gap-2">
+              <button className="lg:rounded-md rounded-sm lg:px-3 lg:py-2 px-2 py-1 bg-primary cursor-pointer flex items-center gap-2 border border-white shadow-[0px_0px_0px01px_var(--primary)] hover:shadow-[0px_0px_10px_1px_var(--primary)] duration-300 transition-all">
+                <span className="lg:text-[16px] text-[12px] text-white font-semibold">
+                  Add to Cart
+                </span>
+                <ShoppingCart size={18} color="white" />
+              </button>
+              <div className="flex items-center gap-2">
+                <button className="lg:rounded-md rounded-sm lg:px-3 lg:py-2 px-2 py-1 bg-primary cursor-pointer flex items-center gap-2 border border-white shadow-[0px_0px_0px01px_var(--primary)] hover:shadow-[0px_0px_10px_1px_var(--primary)] duration-300 transition-all">
+                  <span className="lg:block hidden">Rate</span>
+                  <ThumbsUp size={18} color="white" />
                 </button>
-              ))}
+                <button className="lg:rounded-md rounded-sm lg:px-3 lg:py-2 px-2 py-1 bg-primary cursor-pointer flex items-center gap-2 border border-white shadow-[0px_0px_0px01px_var(--primary)] hover:shadow-[0px_0px_10px_1px_var(--primary)] duration-300 transition-all">
+                  <span className="lg:block hidden">Comment</span>
+                  <MessageCircle size={18} color="white" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* product images */}
+        <div className="relative w-full lg:min-h-[450px] min-h-[350px]">
+          <div className="absolute sm:w-1/2 w-full top-0">
+            {/* product variants */}
+            <div className="absolute sm:flex top-0 left-0 right-0 hidden items-center justify-center">
+              <div
+                className={`md:flex grid gap-2`}
+                style={{
+                  gridTemplateColumns: `repeat(${
+                    product.variants.length === 4 ? 4 : 3
+                  }, 1fr)`,
+                }}
+              >
+                {product.variants.map((v, index) => (
+                  <button
+                    key={index}
+                    onClick={() => {
+                      setSelectedVariant(index);
+                    }}
+                    className={`lg:w-[70px] w-[50px] sm:w-[40px] ${
+                      index === selectedVariant
+                        ? "lg:shadow-[0px_0px_10px_2px_var(--primary)] shadow-[0px_0px_5px_1px_var(--primary)]"
+                        : ""
+                    } bg-white hover:shadow-[0px_0px_10px_2px_var(--primary)] dark:bg-black relative aspect-square border border-primary rounded-md cursor-pointer transition-all duration-300`}
+                  >
+                    <Image
+                      src={`/sneakers-${index + 1}.png`}
+                      alt={product.title}
+                      fill
+                    />
+                  </button>
+                ))}
+              </div>
             </div>
 
-            {/* left right buttons */}
-            <div className="absolute top-[50%] left-0 right-0 -translate-y-[50%] h-10 z-20">
-              <button
-                disabled={selectedVariant === 0}
-                onClick={() => {
-                  setSelectedVariant(selectedVariant - 1);
-                }}
-                className={`absolute left-0 trasnition-transform duration-200 flex items-center justify-center p-1.5 rounded-[50%] aspect-square bg-black/50 dark:bg-white/50 ${
-                  selectedVariant === 0
-                    ? "opacity-50 hover:scale-100 cursor-default"
-                    : "opacity-100 hover:scale-110 cursor-pointer"
-                }`}
-              >
-                <ArrowLeft
-                  size={28}
-                  color={theme === "light" ? "white" : "black"}
-                />
-              </button>
-              <button
-                disabled={selectedVariant === product.variants.length - 1}
-                onClick={() => {
-                  setSelectedVariant(selectedVariant + 1);
-                }}
-                className={`absolute right-0 trasnition-all duration-200 flex items-center justify-center p-1.5 rounded-[50%] aspect-square bg-black/50 dark:bg-white/50 ${
-                  selectedVariant === product.variants.length - 1
-                    ? "opacity-50 hover:scale-100 cursor-default"
-                    : "opacity-100 hover:scale-110 cursor-pointer"
-                }`}
-              >
-                <ArrowRight
-                  size={28}
-                  color={theme === "light" ? "white" : "black"}
-                />
-              </button>
-            </div>
-
-            {/* product images */}
-            <div className="mt-20 relative min-h-[350px] w-full flex items-center justify-center">
+            <div className="sm:mt-20 mt-10 relative lg:min-h-[350px] min-h-[200px] w-full flex items-center justify-center">
+              {/* product images */}
               {product.variants.map((v, index) => {
-                const translateX = (selectedVariant - index) * -42;
+                const translateX =
+                  (selectedVariant - index) * (width > 640 ? -42 : -60);
                 const diff = Math.abs(selectedVariant - index);
 
                 return (
                   <div
                     key={index}
-                    className={`absolute z-10 aspect-square w-[300px] transition-all duration-1000`}
+                    className={`absolute z-10 aspect-square lg:w-[300px] md:w-[250px] sm:w-[200px] w-[200px] transition-all duration-1000`}
                     style={{
                       transform:
                         diff <= 1
@@ -172,6 +245,43 @@ const ProductIdClient = () => {
                 );
               })}
 
+              {/* left right buttons */}
+              <div className="absolute top-[50%] left-0 right-0 -translate-y-[50%] h-10 z-20">
+                <button
+                  disabled={selectedVariant === 0}
+                  onClick={() => {
+                    setSelectedVariant(selectedVariant - 1);
+                  }}
+                  className={`absolute left-0 trasnition-transform duration-200 flex items-center justify-center p-1.5 rounded-[50%] aspect-square bg-black/50 dark:bg-white/50 ${
+                    selectedVariant === 0
+                      ? "opacity-50 hover:scale-100 cursor-default"
+                      : "opacity-100 hover:scale-110 cursor-pointer"
+                  }`}
+                >
+                  <ArrowLeft
+                    size={28}
+                    color={theme === "light" ? "white" : "black"}
+                  />
+                </button>
+                <button
+                  disabled={selectedVariant === product.variants.length - 1}
+                  onClick={() => {
+                    setSelectedVariant(selectedVariant + 1);
+                  }}
+                  className={`absolute right-0 trasnition-all duration-200 flex items-center justify-center p-1.5 rounded-[50%] aspect-square bg-black/50 dark:bg-white/50 ${
+                    selectedVariant === product.variants.length - 1
+                      ? "opacity-50 hover:scale-100 cursor-default"
+                      : "opacity-100 hover:scale-110 cursor-pointer"
+                  }`}
+                >
+                  <ArrowRight
+                    size={28}
+                    color={theme === "light" ? "white" : "black"}
+                  />
+                </button>
+              </div>
+
+              {/* product behind shadow */}
               <div className="absolute inset-0 flex justify-center items-center">
                 <div
                   className={`h-5/9 w-4/7 rounded-t-[50%] rounded-b-[30%] blur-2xl bg-primary/40 dark:bg-primary/20`}
@@ -180,75 +290,10 @@ const ProductIdClient = () => {
             </div>
           </div>
         </div>
-
-        {/* product info card */}
-        <div className="absolute z-20 h-full right-0 top-0 w-1/2 flex items-center justify-center">
-          <div className="p-5 mt-5 flex flex-col gap-1 rounded-2xl dark:shadow-[0px_0px_15px_1px_#ffffff50] shadow-[0px_0px_15px_1px_#00000080] backdrop-blur-xs bg-white/70 dark:bg-black/70">
-            <div className="font-bold text-2xl">{product.title}</div>
-            <div className="flex items-center justify-between gap-2">
-              <div className="text-3xl font-bold">{product.price}$</div>
-              <div className="flex items-center gap-2 bg-primary rounded-full px-2">
-                <span className="font-bold text-2xl text-white">
-                  {product.rating}
-                </span>
-                <Star color="yellow" size={24} fill="yellow" />
-              </div>
-            </div>
-            <div>
-              <div>Choose a size</div>
-              <div className="flex gap-1 items-center mb-1">
-                {product.sizes.map((size, index) => {
-                  return (
-                    <button
-                      className="px-1 py-0.5 min-w-[30px] border border-black/50 dark:border-white/50 rounded-md"
-                      key={index}
-                    >
-                      {size}
-                    </button>
-                  );
-                })}
-              </div>
-            </div>
-            <div>
-              <div>Choose a color</div>
-              <div className="flex items-center gap-1">
-                {product.colors.map((color, index) => {
-                  return (
-                    <button
-                      style={{ backgroundColor: `${color}` }}
-                      className={`w-[25px] aspect-square border border-black/50 dark:border-white/50 rounded-md`}
-                      key={index}
-                    ></button>
-                  );
-                })}
-              </div>
-            </div>
-            <div className="flex items-center justify-between gap-2 font-bold text-lg">
-              <span>
-                Delivery: <span className="text-primary">Free</span>
-              </span>
-              <span>For {product.gender}</span>
-            </div>
-            <div className="flex items-center gap-2">
-              <Button className="flex items-center gap-2 border border-white shadow-[0px_0px_0px01px_var(--primary)] hover:shadow-[0px_0px_10px_1px_var(--primary)] duration-300 transition-all">
-                <span>Add to Cart</span>
-                <ShoppingCart size={16} color="white" />
-              </Button>
-              <Button className="flex items-center gap-2 border border-white shadow-[0px_0px_0px01px_var(--primary)] hover:shadow-[0px_0px_10px_1px_var(--primary)] duration-300 transition-all">
-                <span>Rate</span>
-                <ThumbsUp size={16} color="white" />
-              </Button>
-              <Button className="flex items-center gap-2 border border-white shadow-[0px_0px_0px01px_var(--primary)] hover:shadow-[0px_0px_10px_1px_var(--primary)] duration-300 transition-all">
-                <span>Comment</span>
-                <MessageCircle size={16} color="white" />
-              </Button>
-            </div>
-          </div>
-        </div>
       </div>
 
       {/* product description */}
-      <div className="mt-20 mb-4">
+      <div className="md:mt-20 sm:mt-12 mb-4">
         <div className="text-xl font-bold mb-2">Description</div>
         <div className="font-semibold">{product.description}</div>
       </div>
