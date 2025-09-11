4cbde7bd4a57505b4b1db9b3895efbc7be868762
Muhammad0830 <abduqayumovm1@gmail.com>
2025-09-11 12:01:55 +0500

    new changes (connection between Render and Aiven)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app";
-import { config } from "./config";
 
-const PORT = Number(config.port);
+const PORT = Number(process.env.PORT) || 3001;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`server is running on port ${PORT}`);
 });
