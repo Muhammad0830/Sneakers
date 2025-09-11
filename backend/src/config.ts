3029b3507c4877f6fb7293fdab8aa29bf00b68da
Muhammad0830 <abduqayumovm1@gmail.com>
2025-09-11 12:34:46 +0500

    bugs on connecting render to aiven

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,9 +1,17 @@
+import fs from "fs";
+import path from "path";
+
 export const config = {
-    port: Number(process.env.DB_PORT),
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    jwtSecret: process.env.JWT_SECRET,
-    ssl: { rejectUnauthorized: true }, 
-}
+  serverPort: Number(process.env.PORT) || 10000,
+  db: {
+    host: process.env.DB_HOST!,
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_DATABASE!,
+    port: Number(process.env.DB_PORT) || 3306,
+    ssl: {
+      ca: fs.readFileSync(path.join(__dirname, "../CA_CERT/ca.pem")),
+    },
+  },
+  jwtSecret: process.env.JWT_SECRET!,
+};
