677b7f9ffccd68defa5ea31cf67ab17028004a75
Muhammad0830 <abduqayumovm1@gmail.com>
2025-09-12 11:32:34 +0500

    sign up/in button and profile button is done

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -4,8 +4,10 @@ import { Search, ShoppingCart, Globe, Moon, Sun, Menu, X } from "lucide-react";
 import { useTranslations } from "next-intl";
 import LinkComponent from "./ui/Link";
 import SimpleButton from "@/components/ui/SimpleButton";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useTheme } from "next-themes";
+import { useAuth } from "@/context/AuthContext";
+import { cn } from "@/lib/utils";
 
 const logo = "SNEAKER";
 
@@ -38,6 +40,9 @@ const NavBar = () => {
   const [pathName, setPathName] = useState<string | null>(null);
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+  const { user } = useAuth();
+  const router = useRouter();
+  const [width, setWidth] = useState<number | null>(null);
 
   useEffect(() => setMounted(true), []);
 
@@ -49,10 +54,11 @@ const NavBar = () => {
   useEffect(() => {
     const handleResize = () => {
       const width = window.innerWidth;
+      setWidth(width);
       if (width < 640) setSize(12); // mobile
       else if (width < 768) setSize(16); // sm
-      else if (width < 1024) setSize(20); // md
-      else setSize(24); // lg+
+      else if (width < 1024) setSize(18); // md
+      else setSize(20); // lg+
     };
 
     handleResize();
@@ -62,17 +68,24 @@ const NavBar = () => {
 
   if (!mounted) return null;
 
+  const profileText = user?.user.name
+    .split(" ")
+    .map((n, index) => (index < 2 ? n[0] : null)) // get first letters of first two words
+    .filter(Boolean) // remove null values
+    .join("")
+    .toUpperCase();
+
   return (
     <header className="md:px-4 z-50 fixed left-0 right-0 top-0 bg-varWhite/50 backdrop-blur-xs py-4 lg:px-6 px-4 flex items-center justify-between shadow-gray-500/20 shadow-lg">
       <div className="flex-1 flex sm:justify-center justify-between lg:gap-10 sm:gap-4 gap-6 items-center">
         {/* left side Links | Desktop */}
         <nav className="flex-1 sm:flex lg:gap-10 sm:gap-4 gap-6 justify-end hidden">
-          <div className="lg:text-xl text-xl">
+          <div className="lg:text-lg text-md">
             <LinkComponent animated={"home" === pathName} href="/home">
               {t("home")}
             </LinkComponent>
           </div>
-          <div className="lg:text-xl text-xl">
+          <div className="lg:text-lg text-md">
             <LinkComponent animated={"shop" === pathName} href="/shop">
               {t("shop")}
             </LinkComponent>
@@ -87,60 +100,110 @@ const NavBar = () => {
         <div className="flex-1 flex lg:gap-6 sm:gap-4 gap-6 justify-end sm:justify-between items-center">
           {/* right side Links | Desktop*/}
           <nav className="sm:flex hidden lg:gap-10 sm:gap-4 gap-6">
-            <div className="lg:text-xl text-xl">
+            <div className="lg:text-lg text-md">
               <LinkComponent animated={"contact" === pathName} href="/contact">
                 {t("contact")}
               </LinkComponent>
             </div>
-            <div className="lg:text-xl text-xl">
-              <LinkComponent animated={"signup" === pathName} href="/auth?mode=signup">
-                {t("signup")}
-              </LinkComponent>
+            <div className="lg:text-lg text-md">
+              {user ? (
+                <LinkComponent animated={"Cart" === pathName} href="/cart">
+                  {t("Cart")}
+                </LinkComponent>
+              ) : (
+                <LinkComponent
+                  animated={"auth" === pathName}
+                  href="/auth?mode=signup"
+                >
+                  {t("signup")}
+                </LinkComponent>
+              )}
             </div>
           </nav>
 
           {/* icons | Desktop */}
-          <div className="lg:flex gap-1.5 bg-varWhite rounded-full px-1 py-1 hidden items-center">
-            <div className="p-1 cursor-pointer rounded-full hover:bg-varBlack/5">
-              <Search size={size} color="var(--color-varBlack)" />
-            </div>
-            <div className="p-1 cursor-pointer rounded-full hover:bg-varBlack/5">
-              <ShoppingCart size={size} color="var(--color-varBlack)" />
-            </div>
-            <div className="p-1 cursor-pointer rounded-full hover:bg-varBlack/5">
-              <Globe size={size} color="var(--color-varBlack)" />
-            </div>
-            <button
-              onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-              className="p-1 overflow-hidden cursor-pointer relative rounded-full hover:bg-varBlack/5"
-            >
-              <div
-                className={`relative ${
-                  theme === "dark" ? "-translate-y-[120%]" : "-translate-y-[0%]"
-                } transition-transform duration-300`}
-              >
-                <Moon size={size} color="var(--color-varBlack)" />
+          <div className="flex items-center justify-end gap-4">
+            <div className="lg:flex gap-1 bg-varWhite rounded-full px-1 py-1 hidden items-center">
+              <div className="p-1 cursor-pointer rounded-full hover:bg-varBlack/5">
+                <Search size={size} color="var(--color-varBlack)" />
               </div>
-              <div
-                className={`absolute top-0 bottom-0 flex items-center justify-center ${
-                  theme === "dark" ? "translate-y-[0%]" : "translate-y-[120%]"
-                } transition-transform duration-300`}
-              >
-                <Sun size={size} color="var(--color-varBlack)" />
+              {user ? null : (
+                <div className="p-1 cursor-pointer rounded-full hover:bg-varBlack/5">
+                  <ShoppingCart size={size} color="var(--color-varBlack)" />
+                </div>
+              )}
+              <div className="p-1 cursor-pointer rounded-full hover:bg-varBlack/5">
+                <Globe size={size} color="var(--color-varBlack)" />
               </div>
-            </button>
+              <button
+                onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+                className="p-1 overflow-hidden cursor-pointer relative rounded-full hover:bg-varBlack/5"
+              >
+                <div
+                  className={`relative ${
+                    theme === "dark"
+                      ? "-translate-y-[120%]"
+                      : "-translate-y-[0%]"
+                  } transition-transform duration-300`}
+                >
+                  <Moon size={size} color="var(--color-varBlack)" />
+                </div>
+                <div
+                  className={`absolute top-0 bottom-0 flex items-center justify-center ${
+                    theme === "dark" ? "translate-y-[0%]" : "translate-y-[120%]"
+                  } transition-transform duration-300`}
+                >
+                  <Sun size={size} color="var(--color-varBlack)" />
+                </div>
+              </button>
+            </div>
+            {user && (width ? width >= 1024 : true) ? (
+              <button
+                onClick={() => router.push("/profile")}
+                className="aspect-square h-9 rounded-full flex items-center justify-center cursor-pointer bg-primary"
+              >
+                <span
+                  className={cn(
+                    "text-white font-bold",
+                    profileText?.length === 1 ? "text-[16px]" : "text-[14px]"
+                  )}
+                >
+                  {profileText}
+                </span>
+              </button>
+            ) : null}
           </div>
 
           {/* just for mobile and Tablet */}
-          <div className="text-md sm:hidden">
-            <LinkComponent href={"/auth?mode=signup"}>{t("signup")}</LinkComponent>
+          <div className="flex items-center gap-2 justify-end">
+            {user && (width ? width <= 1024 : true) ? (
+              <button
+                onClick={() => router.push("/profile")}
+                className="aspect-square h-9 rounded-full flex items-center justify-center cursor-pointer bg-primary"
+              >
+                <span
+                  className={cn(
+                    "text-white font-bold",
+                    profileText?.length === 1 ? "text-[16px]" : "text-[14px]"
+                  )}
+                >
+                  {profileText}
+                </span>
+              </button>
+            ) : (
+              <div className="text-md sm:hidden">
+                <LinkComponent href={"/auth?mode=signup"}>
+                  {t("signup")}
+                </LinkComponent>
+              </div>
+            )}
+            <SimpleButton
+              onClick={() => setOpen((prev) => !prev)}
+              className="lg:hidden flex justify-center items-center"
+            >
+              <Menu size={30} color="var(--color-varBlack)" />
+            </SimpleButton>
           </div>
-          <SimpleButton
-            onClick={() => setOpen((prev) => !prev)}
-            className="lg:hidden flex justify-center items-center"
-          >
-            <Menu size={30} color="var(--color-varBlack)" />
-          </SimpleButton>
         </div>
       </div>
 
@@ -194,9 +257,15 @@ const NavBar = () => {
                     key={index}
                     onClick={() => setOpen(false)}
                     className="md:text-3xl text-2xl"
-                    href={item.href}
+                    href={
+                      item.href.startsWith("/auth") && user
+                        ? "/cart"
+                        : item.href
+                    }
                   >
-                    {item.name}
+                    {item.href.startsWith("/auth") && user
+                      ? t("Cart")
+                      : item.name}
                   </LinkComponent>
                 );
               }
---
+++ frontend/src/components/NavChildrenFooter.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import { useAuth } from "@/context/AuthContext";
+
+const NavChildrenFooter = ({ children }: { children: React.ReactNode }) => {
+  const { loading } = useAuth();
+
+  if (loading) return null;
+
+  return (
+    <div>
+      <NavBar />
+      <div className="pt-18 flex-1 overflow-hidden">{children}</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default NavChildrenFooter;
